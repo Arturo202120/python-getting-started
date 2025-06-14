5fcfff4daa62a99131c2c1534d52fff20fcd9ade
Arturo202120 <avallejos17@alumno.uned.es>
2025-06-14 00:07:27 -0500

    Update urls.py
    Signed-off-by: Arturo202120 <avallejos17@alumno.uned.es>

--- gettingstarted/urls.py
+++ gettingstarted/urls.py
@@ -1,19 +1,6 @@
-"""
-URL configuration for gettingstarted project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-"""
 from django.urls import path
-
-# Forma más limpia de importar: Traemos cada función que necesitamos directamente
-from hello.views import webhook, index, db 
+from hello.views import webhook
 
 urlpatterns = [
-    # El webhook para WhatsApp
     path("webhook", webhook, name="webhook"),
-    
-    # Las rutas originales del proyecto de ejemplo
-    path("", index, name="index"),
-    path("db", db, name="db"),
 ]
